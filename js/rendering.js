2d82b52240c5ecf4d270180f0df05fc3abcfd627
Alexandr Avilov <aaavilovspb@gmail.com>
2022-07-23 16:38:33 +0300

    Отрисуй меня полностью (Часть  1)

---
+++ js/rendering.js
@@ -0,0 +1,18 @@
+import {createPhotoDescriptions} from './data.js';
+
+const pictureBlock = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const publicationsArray = createPhotoDescriptions();
+
+const publicationsFragment = document.createDocumentFragment();
+
+publicationsArray.forEach(({url, likes, comments}) => {
+  const publicationElement = pictureTemplate.cloneNode(true);
+  publicationElement.querySelector('.picture__img').src = url;
+  publicationElement.querySelector('.picture__comments').textContent = comments.length;
+  publicationElement.querySelector('.picture__likes').textContent = likes;
+  publicationsFragment.appendChild(publicationElement);
+});
+
+pictureBlock.appendChild(publicationsFragment);
