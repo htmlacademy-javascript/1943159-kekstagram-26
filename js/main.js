e5b51a5c1b1d04cbcae18839b9c9f021affcdf2b
Alexandr Avilov <aaavilovspb@gmail.com>
2022-07-24 16:12:07 +0300

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ import './rendering.js';
 import './full-size.js';
 import './form.js';
 import './image-editing.js';
+import './upload-messages.js';
+import {createPublicationsArray} from './rendering.js';
+import {setUserFormSubmit} from './form.js';
+import {overlayClose} from './form.js';
+import {getData} from './api.js';
+
+
+getData(createPublicationsArray);
+
+setUserFormSubmit(overlayClose);
